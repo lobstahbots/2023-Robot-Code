9554cc84fcb1ca423b72be5523f3cd7661c77048
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2023-03-05 18:05:18 -0500

    fix errors

--- src/main/java/frc/robot/auton/AutonGenerator.java
+++ src/main/java/frc/robot/auton/AutonGenerator.java
@@ -12,6 +12,7 @@ import com.pathplanner.lib.PathPlannerTrajectory;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import frc.robot.ScoringPosition;
 import frc.robot.Constants.AutonConstants;
