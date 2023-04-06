18f67f295cc71ed26b27c2212b6d7597874ed651
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2023-04-06 16:13:51 -0400

    add camera

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,9 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.CvSource;
+import edu.wpi.first.cscore.UsbCamera;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to each mode, as
@@ -27,6 +30,8 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     m_robotContainer = new RobotContainer();
     m_robotContainer.initGyro();
+    UsbCamera driverCam = CameraServer.startAutomaticCapture();
+    driverCam.setResolution(640, 480);
     m_robotContainer.configureSmartDash();
   }
 
